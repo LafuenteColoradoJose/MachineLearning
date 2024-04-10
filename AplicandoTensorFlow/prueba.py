229c5c23995e9c4657db5285763e15133f086f5e
LafuenteColoradoJose <joselafuente77@gmail.com>
2024-04-10 12:10:12 +0200

    Add exercices

--- AplicandoTensorFlow/prueba.py
+++ AplicandoTensorFlow/prueba.py
@@ -6,19 +6,31 @@ Created on Tue Apr  9 12:49:32 2024
 @author: pp
 """
 
+# 1 - Para instalar TensorFlow
+# ejecutamos en la terminal: pip install tensorflow
+
+# 2 - Importamos las librerías necesarias
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# 3 - Obtenemos los datos del CSV
 data = pd.read_csv('../AplicandoTensorFlow/celsius_a_fahrenheit.csv')
+print(data)
 celsius = np.array(data['Celsius'])
 fahrenheit = np.array(data['Fahrenheit'])
 
-# creamos el modelo
+
+# 4 - Compilación del modelo
+
+# Creamos el modelo
+
+# ejemplo con solo una capa
 # capa = tf.keras.layers.Dense(units=1, input_shape=[1])
 # modelo = tf.keras.Sequential([capa])
 
+# ejemplo con 2 capas ocultas
 oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
 oculta2 = tf.keras.layers.Dense(units=3)
 salida = tf.keras.layers.Dense(units=1)
@@ -30,26 +42,25 @@ modelo.compile(
 )
 
 print('Comenzando entrenamiento...')
-print()
+print("\n")
 historial = modelo.fit(celsius, fahrenheit, epochs=500, verbose=False)
 print('Modelo entrenado')
-print()
+print("\n")
 
-#imprimimos la función de pérdida
+# Imprimimos la función de pérdida
 plt.xlabel('# Epoca')
 plt.ylabel('Magnitud de pérdida')
 plt.plot(historial.history['loss'])
 
-#hacemos una predicción (280C x 9)/5 + 32 = 536 F
+# Hacemos una predicción (35C x 9)/5 + 32 = 95F
+print("\n")
 print('Predicción')
-print(modelo.predict([280.0]))
+print(modelo.predict([35.0]))
+print("\n")
 
-# imprimimos los pesos de la capa
+# Imprimimos los pesos de la capa/s
 # print('Estos son los pesos de la capa: {}'.format(capa.get_weights()))
 print('Estos son los pesos de la capa oculta 1: {}'.format(oculta1.get_weights()))
 print('Estos son los pesos de la capa oculta 2: {}'.format(oculta2.get_weights()))
 print('Estos son los pesos de la capa de salida: {}'.format(salida.get_weights()))
-print()
-
-
-
+print("\n")
